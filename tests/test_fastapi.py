5daaa6338cf6e17d0d9be4850e4e00b3ae1d1eea
graffkevin <kevin.graff1@gmail.com>
2023-11-03 15:07:07 +0100

    finzalize generate list name model

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -9,18 +9,21 @@ from dataclasses import dataclass
 @pytest.fixture()
 def app():
     from tracktolib.api import JSONSerialResponse
+
     return FastAPI(
-        openapi_tags=[{'name': 'foo', 'description': 'bar'}],
-        default_response_class=JSONSerialResponse
+        openapi_tags=[{"name": "foo", "description": "bar"}],
+        default_response_class=JSONSerialResponse,
     )
 
 
 def test_get_return_type():
     from tracktolib.api import _get_return_type, Response
+
     class ReturnBar(BaseModel):
         foo: int
 
-    async def bar_endpoint() -> Response[ReturnBar | None]: ...
+    async def bar_endpoint() -> Response[ReturnBar | None]:
+        ...
 
     assert _get_return_type(bar_endpoint) == ReturnBar | None
 
@@ -48,10 +51,8 @@ def test_add_endpoint(app):
         bar: str
 
     @endpoint.get()
-    async def foo_endpoint(
-            foo: int = Depends(compute_sum)
-    ) -> Response[ReturnFoo]:
-        return {'foo': foo}
+    async def foo_endpoint(foo: int = Depends(compute_sum)) -> Response[ReturnFoo]:
+        return {"foo": foo}
 
     depends_called = False
 
@@ -61,44 +62,43 @@ def test_add_endpoint(app):
 
     @endpoint.post(dependencies=[fastapi.Depends(foo_depends)])
     async def foo2_endpoint() -> Response[list[ReturnFoo]]:
-        return [{'foo': 1}]
+        return [{"foo": 1}]
 
     @endpoint2.get(status_code=status.HTTP_202_ACCEPTED)
     async def bar_endpoint(return_empty: bool = False) -> Response[ReturnBar | None]:
-        return {'foo': 1} if not return_empty else None
+        return {"foo": 1} if not return_empty else None
 
-    @endpoint3.get(path='foo/{foo}/bar/{bar}',
-                   model=list[ReturnFooBar])
-    async def path_endpoint(foo: int,
-                            bar: str):
-        return [{'foo': foo, 'bar': bar}]
+    @endpoint3.get(path="foo/{foo}/bar/{bar}", model=list[ReturnFooBar])
+    async def path_endpoint(foo: int, bar: str):
+        return [{"foo": foo, "bar": bar}]
 
     router = APIRouter()
 
-    add_endpoint('/foo', router, endpoint)
-    add_endpoint('/bar', router, endpoint2)
-    add_endpoint('/path_endpoint', router, endpoint3)
+    add_endpoint("/foo", router, endpoint)
+    add_endpoint("/bar", router, endpoint2)
+    add_endpoint("/path_endpoint", router, endpoint3)
 
     app.include_router(router)
 
     with TestClient(app) as client:
-        resp = client.get('/foo')
-        resp2 = client.post('/foo')
-        resp3 = client.get('/bar')
-        resp4 = client.get('/bar', params={'return_empty': True})
-        resp5 = client.get('/path_endpoint/foo/2/bar/baz')
+        resp = client.get("/foo")
+        resp2 = client.post("/foo")
+        resp3 = client.get("/bar")
+        resp4 = client.get("/bar", params={"return_empty": True})
+        resp5 = client.get("/path_endpoint/foo/2/bar/baz")
 
-        doc_resp = client.get('/openapi.json')
+        doc_resp = client.get("/openapi.json")
 
-    assert_equals(resp.json(), {'foo': 2})
-    assert_equals(resp2.json(), [{'foo': 1}])
-    assert resp3.json() == {'foo': 1}
+    assert_equals(resp.json(), {"foo": 2})
+    assert_equals(resp2.json(), [{"foo": 1}])
+    assert resp3.json() == {"foo": 1}
     assert resp3.status_code == status.HTTP_202_ACCEPTED
     assert resp4.json() is None
-    assert resp5.json() == [{'foo': 2, 'bar': 'baz'}]
+    assert resp5.json() == [{"foo": 2, "bar": "baz"}]
     assert doc_resp.status_code == status.HTTP_200_OK
 
     assert depends_called
+
 
 def test_camelcase_model(app):
     from tracktolib.api import add_endpoint, Endpoint, CamelCaseModel
@@ -114,18 +114,17 @@ def test_camelcase_model(app):
 
     @endpoint.post(model=OutputModel)
     async def foo_endpoint(data: InputModel):
-        return {'foo_bar': data.foo_bar}
+        return {"foo_bar": data.foo_bar}
 
     router = APIRouter()
 
-    add_endpoint('/foo', router, endpoint)
+    add_endpoint("/foo", router, endpoint)
     app.include_router(router)
 
     with TestClient(app) as client:
-        resp = client.post('/foo', json={'foo_bar': 1})
+        resp = client.post("/foo", json={"foo_bar": 1})
 
-    assert_equals(resp.json(), {'fooBar': 1})
-
+    assert_equals(resp.json(), {"fooBar": 1})
 
 
 def check_json_serial_types():
@@ -138,49 +137,68 @@ def check_json_serial_types():
     def _json_serial(obj):
         if isinstance(obj, Bar):
             return str(obj.foo)
-        raise TypeError(f'Object of type {type(obj)} is not JSON serializable')
+        raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
 
     class Foo(JSONSerialResponse):
         json_serial = _json_serial
 
 
 def test_update_array_metadata(app):
+    import json
     from fastapi.openapi.utils import get_openapi
     from tracktolib.api import Endpoint, CamelCaseModel, add_endpoint
     from tracktolib.tests import assert_equals
 
-    endpoint_a = Endpoint()
-    endpoint_b = Endpoint()
+    first_endpoint = Endpoint()
+    second_endpoint = Endpoint()
 
     class Foo(CamelCaseModel):
-        foo: int
+        foo_int: int
 
-    @endpoint_a.get(model=list[Foo])
+        class Config:
+            schema_extra = {"example": {"foo_int": 1}}
+
+    class Bar(CamelCaseModel):
+        bar_int: int
+
+        class Config:
+            schema_extra = {"example": {"bar_int": 2}}
+
+    @first_endpoint.get(model=Foo)
     async def foo_endpoint():
-        return [{'foo': 1}, {'foo': 2}]
+        """
+        Nice description
+        """
+        return {"foo_int": 1}
 
-    @endpoint_b.get(model=Foo)
-    async def foobar_endpoint():
-        return {'foo': 1}
+    @second_endpoint.get(model=list[Bar], status_code=200)
+    async def foo_endpoint():
+        """
+        Another nice description
+        """
+        return [{"bar_int": 1}, {"bar_int": 2}]
 
     router = APIRouter()
 
-    add_endpoint('/foo', router, endpoint_a)
-    add_endpoint('/foobar', router, endpoint_b)
+    add_endpoint("/foo", router, first_endpoint)
+    add_endpoint("/bar", router, second_endpoint)
     app.include_router(router)
 
     with TestClient(app) as client:
-        resp_a = client.get('/foo')
-        resp_b = client.get('/foobar')
-    assert_equals(resp_a.json(), [{'foo': 1}, {'foo': 2}])
-    assert_equals(resp_b.json(), {'foo': 1})
+        resp_foo = client.get("/foo")
+        resp_bar = client.get("/bar")
+    assert_equals(resp_foo.json(), {"fooInt": 1})
+    assert_equals(resp_bar.json(), [{"barInt": 1}, {"barInt": 2}])
 
     openapi_schema = get_openapi(title="title", version="0.1", routes=app.routes)
-    schema_a = openapi_schema["paths"]["/foo"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]
-    schema_b = openapi_schema["components"]["schemas"]["Foo"]
-    title_response_a = schema_a.get("title", "No title found")
-    title_response_b = schema_b.get("title", "No title found")
-    assert_equals(title_response_a, "Array[Foo]")
-    assert_equals(title_response_b, "Foo")
 
-
+    schema_foo = openapi_schema["paths"]["/foo"]["get"]["responses"]["200"]["content"][
+        "application/json"
+    ]["schema"]
+    schema_bar = openapi_schema["paths"]["/bar"]["get"]["responses"]["200"]["content"][
+        "application/json"
+    ]["schema"]
+    title_response_foo = schema_foo.get("title", "No title found")
+    title_response_bar = schema_bar.get("title", "No title found")
+    assert_equals(title_response_foo, "Foo")
+    assert_equals(title_response_bar, "Array[Bar]")
