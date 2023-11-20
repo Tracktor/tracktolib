8943c15a2bf8d864735dd0d5254c239710cc0621
Julien Brayere <julien.brayere@obitrain.com>
2023-11-20 16:57:47 +0100

    feat: adding download text file support

--- tracktolib/http.py
+++ tracktolib/http.py
@@ -1,32 +1,75 @@
 import typing
-from typing import BinaryIO, Callable
+from typing import BinaryIO, Callable, TextIO
 
 try:
     import httpx
     from httpx._types import QueryParamTypes
 except ImportError:
-    raise ImportError('Please install httpx or tracktolib with "http" to use this module')
+    raise ImportError(
+        'Please install httpx or tracktolib with "http" to use this module'
+    )
 
 MB_1 = 1024 * 1024
 
 
-async def download_file(url: str,
-                        client: httpx.AsyncClient,
-                        output_file: BinaryIO,
-                        *,
-                        chunk_size: int = MB_1 * 10,
-                        on_chunk_received: Callable[[bytes], None] | None = None,
-                        on_response: Callable[[httpx.Response], None] | None = None,
-                        params: QueryParamTypes | None = None,
-                        headers: dict[str, str] | None = None):
+@typing.overload
+async def download_file(
+    url: str,
+    client: httpx.AsyncClient,
+    output_file: TextIO,
+    *,
+    chunk_size: int,
+    on_chunk_received: Callable[[str], None] | None,
+    on_response: Callable[[httpx.Response], None] | None,
+    params: QueryParamTypes | None,
+    headers: dict[str, str] | None,
+):
+    ...
+
+
+@typing.overload
+async def download_file(
+    url: str,
+    client: httpx.AsyncClient,
+    output_file: BinaryIO,
+    *,
+    chunk_size: int,
+    on_chunk_received: Callable[[bytes], None] | None,
+    on_response: Callable[[httpx.Response], None] | None,
+    params: QueryParamTypes | None,
+    headers: dict[str, str] | None,
+):
+    ...
+
+
+async def download_file(
+    url: str,
+    client: httpx.AsyncClient,
+    output_file: BinaryIO | TextIO,
+    *,
+    chunk_size: int = MB_1 * 10,
+    on_chunk_received: Callable[[typing.Any], None] | None = None,
+    on_response: Callable[[httpx.Response], None] | None = None,
+    params: QueryParamTypes | None = None,
+    headers: dict[str, str] | None = None,
+):
     """
     on_chunk_received: Useful to compute the hash for install or display progress
     """
-    async with client.stream('GET', url, params=params, headers=headers) as r:
+
+    async with client.stream("GET", url, params=params, headers=headers) as r:
         if on_response:
             on_response(r)
-        async for data in r.aiter_bytes(chunk_size=chunk_size):
-            output_file.write(data)
+        match output_file:
+            case TextIO():
+                _iter_fn = r.aiter_text
+            case BinaryIO():
+                _iter_fn = r.aiter_bytes
+            case _:
+                raise NotImplementedError("output_file must be a TextIO or a BinaryIO")
+
+        async for data in _iter_fn(chunk_size=chunk_size):
+            output_file.write(data)  # type: ignore
             if on_chunk_received:
                 on_chunk_received(data)
 
